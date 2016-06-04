73bb0a4b2855c654c213d34d0489e6251c411cca
gejw <gejw0623@yeah.net>
2016-06-04 16:02:38 +0800

    updat demo

--- demo/mynt-demo/Pods/MYNT-SDK-IOS/MYNT-SDK-IOS/STMyntBluetooth.h
+++ demo/mynt-demo/Pods/MYNT-SDK-IOS/MYNT-SDK-IOS/STMyntBluetooth.h
@@ -9,69 +9,17 @@
 #import <Foundation/Foundation.h>
 #import "STMynt.h"
 
-@class STMyntBluetooth;
-@protocol STMyntBluetoothDelegate <NSObject>
-
-@optional
-
-/**
- *  Invoked when the central manager's state has been updated.
- *
- *  @param myntBluetooth
- *  @param state         the current state of the bluetooth
- */
-- (void)myntBluetooth:(STMyntBluetooth * _Nonnull)myntBluetooth didUpdateState:(CBCentralManagerState)state;
-
-/**
- *  Invoked when the mynt has been discoverd.
- *
- *  @param myntBluetooth
- *  @param mynt
- */
-- (void)myntBluetooth:(STMyntBluetooth * _Nonnull)myntBluetooth didDiscoverMynt:(STMynt * _Nonnull)mynt;
-
-/**
- *  Invoked when the mynt has been discoverd timeout.
- *
- *  @param myntBluetooth
- *  @param mynt
- */
-- (void)myntBluetooth:(STMyntBluetooth * _Nonnull)myntBluetooth didDiscoverTimeoutMynt:(STMynt * _Nonnull)mynt;
-
-/**
- *  Invoked if you need filter the mynts.
- *
- *  @param myntBluetooth
- *  @param sn            the sn of the mynt when the mynt is discoverd.
- *
- *  @return if this mynt need filter(don't ues it), so you can need true.
- */
-- (BOOL)myntBluetooth:(STMyntBluetooth * _Nonnull)myntBluetooth didFilterMyntWithSn:(NSString * _Nonnull)sn;
-
-@end
-
 @interface STMyntBluetooth : NSObject
 // Flag to determine if the manager is scanning
 @property (nonatomic, assign) BOOL isScanning;
 // Help others find the mynt
 @property (nonatomic, assign) BOOL reportLost;
-// TODO: userid
-@property (nonatomic, strong, nullable) NSString *userID;
 // The current state of the bluetooth
 @property (nonatomic, assign) CBCentralManagerState centralState;
 // Delegate
 @property (nonatomic, weak, nullable) id<STMyntBluetoothDelegate> delegate;
 
 + (instancetype _Nonnull)sharedInstance;
-
-/**
- *  Get mynt with sn
- *
- *  @param sn the sn of the mynt
- *
- *  @return mynt
- */
-- (STMynt * _Nullable)findMyntWithSn:(NSString * _Nonnull)sn;
 
 /**
  *  Start scanning for mynts.
@@ -84,5 +32,21 @@
  *  Stop scanning for mynts.
  */
 - (void)stopScan;
+
+/**
+ *  Retrieves all mynts that are connected to the system (not only in app)
+ *
+ *  @return
+ */
+- (NSArray <STMynt *> * _Nullable)retrieveConnectedMynts;
+
+/**
+ *  get the mynt with sn
+ *
+ *  @param sn the sn of the mynt
+ *
+ *  @return
+ */
+- (STMynt * _Nullable)findMyntWithSn:(NSString *_Nonnull)sn;
 
 @end
